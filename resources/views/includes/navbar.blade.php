d55a5cb6b9bfced633a1b9b3a22e6a7fcca3d42a
isnanar <synting123@gmail.com>
2021-06-01 17:15:13 +0700

    add login feature

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,6 +2,6 @@
     Nama Pengguna <em class="fas fa-circle px-2" style="font-size:10px"></em> Administrator
     <span class="float-right">
         <a href="#" class="btn navbar-btn mr-3">Akun Pengguna</a>
-        <button class="btn navbar-btn text-danger">Log out</button>
+        <a class="btn navbar-btn text-danger" href="{{ url('/logout') }}"> Log out </a>
     </span>
 </div>
